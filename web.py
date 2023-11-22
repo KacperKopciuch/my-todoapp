749d391d1c49ea5222263230250b1bb072254b3a
Kacper <kacperkopciuch@gmail.com>
2023-11-22 10:28:03 +0100

    added mb

--- web.py
+++ web.py
@@ -13,7 +13,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("Increase your productivity")
+st.write("Increase your productivity!")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
